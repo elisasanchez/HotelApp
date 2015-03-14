c7d5ee316b8ca68b6bce6323649e2a6785a0c0c5
elisasanchez <egsh79@hotmail.com>
2015-03-14 13:37:04 -0600

    x
    x

---
+++ js/api/filetransfer.js
@@ -0,0 +1,23 @@
+//file-transfer
+var transfer = {
+    imgSuccess: function(r){
+        if(r.response == 1){
+            $.mobile.loading( 'hide' );
+            navigator.notification.alert('El registro de sus datos ha sido un éxito.', function(){
+                window.location.href = '#home';
+            }, 'Registro','Aceptar');
+        }
+    },
+    imgError: function(error){
+        alert("An error has occurred: Code = " + error.code);
+    },
+    imgUpload: function(img){
+        var options = new FileUploadOptions();
+        options.fileKey = "foto";
+        options.fileName = "Elisa";
+        options.mimeType = "image/jpeg";
+        
+        var ft = new FileTransfer();
+        ft.upload(img, server.url, transfer.imgSuccess, transfer.imgError, options);
+    }
+};
