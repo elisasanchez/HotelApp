f392ffcd880a5f08593cf05abd51848dce2eefa1
elisasanchez <egsh79@hotmail.com>
2015-03-21 09:51:01 -0600

    x
    con foto

---
+++ js/storage.js
@@ -0,0 +1,13 @@
+//almacenamiento
+var almacenamiento = {
+    createReg: function(nom, id){
+        window.localStorage.setItem('name',nom);
+        window.localStorage.setItem('uuid',id);
+    },
+    isRegistered: function(){
+        if (window.localStorage.getItem('uuid') != undefined && window.localStorage.getItem('uuid') != '')
+            return true;
+        else
+            return false;
+    }
+};
